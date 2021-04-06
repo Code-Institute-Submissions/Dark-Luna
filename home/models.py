5080c22b34838cac627bde9ff7913f466b5ea1d2
ByIlsa <byilsa@aukjevanderwal.nl>
2021-04-06 12:43:13 +0000

    Start fresh database

--- home/models.py
+++ home/models.py
@@ -1,11 +1,2 @@
 from django.db import models
 
-# from embed_video.fields import EmbedVideoField
-
-
-# class Video(models.Model):
-#     title = models.CharField(max_length=100)
-#     url = EmbedVideoField()
-
-#     def __str__(self):
-#         return str(self.title)
