dbc5f680ee0c87e943b9f187fbe0c7d07e3cacc5
ByIlsa <byilsa@aukjevanderwal.nl>
2021-04-08 08:51:07 +0000

    Add apps related to coaching (life, sex, shadow)

--- dark_luna/urls.py
+++ dark_luna/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('why/', include('why.urls')),
     path('how/', include('how.urls')),
+    path('life/', include('life.urls')),
+    path('sex/', include('sex.urls')),
+    path('shadow/', include('shadow.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
