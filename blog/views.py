e70d2469e564ad0e9f0ba3d8e98db0eb27a10362
ByIlsa <byilsa@aukjevanderwal.nl>
2021-04-13 10:11:03 +0000

    Add basic blog post model and registering it

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index(request):
     """ A view to return index page """
 
-    return render(request, 'blog/blog.html')
+    return render(request, 'blog/blog.html', {})
