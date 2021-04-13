79f37f1bd5d75a05a395363e5f218278145eac37
ByIlsa <byilsa@aukjevanderwal.nl>
2021-04-13 11:53:11 +0000

    Add basic admin add blog functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import BlogHomeView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.index, name='blog'),
+    path('', BlogHomeView.as_view(), name='blog'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 
-def index(request):
-    """ A view to return index page """
+# def index(request):
+#     """ A view to return index page """
 
-    return render(request, 'blog/blog.html', {})
+#     return render(request, 'blog/blog.html', {})
+
+class BlogHomeView(ListView):
+    model = Post
+    template = 'blog/blog.html'
