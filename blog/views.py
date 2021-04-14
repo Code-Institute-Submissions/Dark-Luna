7aef1edfb4abae6b7ed1b05ad89a60e161f9479c
ByIlsa <byilsa@aukjevanderwal.nl>
2021-04-14 17:08:35 +0000

    Remove commented out code

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,6 @@ from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
-# def index(request):
-#     """ A view to return index page """
-
-#     return render(request, 'blog/blog.html', {})
-
 class BlogHomeView(ListView):
     model = Post
     template = 'blog/post_list.html'
