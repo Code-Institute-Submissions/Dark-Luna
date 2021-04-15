02363c0ccc2ddec039dd115d19f65ed77260a427
ByIlsa <byilsa@aukjevanderwal.nl>
2021-04-15 13:38:55 +0000

    Add delete function for blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-# from . import views
-from .views import BlogHomeView, BlogDetailView, AddBlogPost
+from .views import (
+     BlogHomeView, BlogDetailView, AddBlogPost, UpdatePostView, DeletePostView)
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', BlogHomeView.as_view(), name='blog'),
-    path('article/<int:pk>', BlogDetailView.as_view(),
-         name='blog-post-detail'),
-    path('add_post/', AddBlogPost.as_view(),
-         name='add-blog-post'),
+     path('', BlogHomeView.as_view(), name='blog'),
+     path('article/<int:pk>', BlogDetailView.as_view(),
+          name='blog-post-detail'),
+     path('add_post/', AddBlogPost.as_view(),
+          name='add-blog-post'),
+     path('article/update/<int:pk>', UpdatePostView.as_view(),
+          name='update-blog-post'),
+     path('article/delete/<int:pk>', DeletePostView.as_view(),
+          name='delete_post'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,33 @@
-# from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Post
 from .forms import PostForm
 
 
 class BlogHomeView(ListView):
     model = Post
-    template = 'blog/post_list.html'
+    template_name = 'blog/post_list.html'
 
 
 class BlogDetailView(DetailView):
     model = Post
-    template = 'blog/post_detail.html'
+    template_name = 'blog/post_detail.html'
 
 
 class AddBlogPost(CreateView):
     model = Post
     form_class = PostForm
-    template = 'blog/post_form.html'
+    template_name = 'blog/post_form.html'
 
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'blog/update_blog.html'
+    fields = ['title', 'tag', 'body']
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'blog/delete_blog.html'
+    success_url = reverse_lazy('blog')
