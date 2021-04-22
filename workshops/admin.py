17851abce3aa45b701ce999fc92dd4d9d082112f
ByIlsa <byilsa@aukjevanderwal.nl>
2021-04-22 09:51:02 +0000

    Add basic model and admin register

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Workshop
 
-# Register your models here.
+
+admin.site.register(Workshop)
+admin.site.register(Category)
--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,38 @@
+""" Models for Workshops App """
 from django.db import models
+from django.contrib.auth.models import User
+# from home.models import LevelCard
+# from resorts.models import Resort
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Lessons Category Model """
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=False)
+
+    def __str__(self):
+        return self.friendly_name
+
+
+class Workshop(models.Model):
+    """ Workshop Model """
+    name = models.CharField(max_length=254)
+    category = models.ForeignKey(
+        'Category', null=True, blank=False, on_delete=models.PROTECT)
+    description = models.TextField(null=True, blank=True)
+    start_date = models.DateField(null=True, blank=False)
+    end_date = models.DateField(null=True, blank=False)
+    start_time = models.TimeField(auto_now=False, auto_now_add=False)
+    end_time = models.TimeField(auto_now=False, auto_now_add=False)
+    participants = models.IntegerField(null=True, blank=False)
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=False)
+    image = models.ImageField(blank=True, null=True,
+                              upload_to="user_uploads/")
+    therapist = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
