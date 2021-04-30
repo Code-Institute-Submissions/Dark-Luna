b56cb866263e574e8daf4d8ab9f3ff3c789ee921
ByIlsa <byilsa@aukjevanderwal.nl>
2021-04-30 07:32:30 +0000

    Deploy to heroku with s3

--- dark_luna/settings.py
+++ dark_luna/settings.py
@@ -62,8 +62,10 @@ INSTALLED_APPS = [
     'django_countries',
     'ckeditor',
     'checkout',
+
+    #Other
     'crispy_forms',
-    # 'storages',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -134,7 +136,7 @@ WSGI_APPLICATION = 'dark_luna.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.getenv:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
@@ -187,6 +189,30 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'BUCKET_S3' in os.getenv:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    # Bucket S3 Congig
+    AWS_STORAGE_BUCKET_NAME = "darkluna"
+    AWS_S3_REGION_NAME = "eu-west-2"
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 # Gmail sending real e-mail settings
